69622059c68e6de0f7da770917bba1c291d4dc2a
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-07-27 23:31:55 +0100

    updated the backend

--- server/schema.sql
+++ server/schema.sql
@@ -1,6 +1,12 @@
 CREATE TABLE barbers (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    user_id INTEGER REFERENCES users(id),
+    name VARCHAR(255) NOT NULL,
+    phone_number TEXT,
+    account_name VARCHAR(255),
+    location VARCHAR(255),
+    barber_shop_name VARCHAR(255),
+    expertise VARCHAR(255)
 );
 
 CREATE TABLE users(
@@ -8,13 +14,16 @@ CREATE TABLE users(
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    account_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role VARCHAR(50) CHECK (role IN ('barber','customer'))
 );
 
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
     barber_id INTEGER REFERENCES barbers(id),
+    style VARCHAR(255) NOT NULL,
     rating INTEGER CHECK (rating >= 1 AND rating <= 5),
     review_text TEXT,
     review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
